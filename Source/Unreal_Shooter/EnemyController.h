a005ef9c593b52572148a12513b6af985aa920a6
Neha Ghanta <55118296+NehaG991@users.noreply.github.com>
2022-08-09 13:13:17 -0500

    Created Enemy and EnemyController CPP and H files

---
+++ Source/Unreal_Shooter/EnemyController.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_SHOOTER_API AEnemyController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	AEnemyController();
+
+	virtual void OnPossess(APawn* InPawn) override;
+
+	UBlackboardComponent* GetBlackboard();
+
+protected:
+	UPROPERTY(BlueprintReadWrite, Category = Behavior)
+		class UBlackboardComponent* BlackboardComponent;
+
+	UPROPERTY(BlueprintReadWrite, Category = Bahavior)
+		class UBehaviorTreeComponent* BehaviorTreeComponent;
+};
